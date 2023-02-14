c8d48a79897c113e8b337e0588e4673c260ec3ce
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2023-02-14 20:14:01 +0300

    first commit

---
+++ screens/Welcome.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+import { Button, Block, Text } from "../components";
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
