92f33455b74578c33b4efc12650bd7c43078dbe0
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2023-02-17 15:13:32 +0300

    err

--- components/Text.js
+++ components/Text.js
@@ -1,98 +1,132 @@
+// just copy this code from the driving repo :)
 import React, { Component } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { Text, StyleSheet } from "react-native";
+
 import { theme } from "../constants";
 
-class Button extends Component {
+export default class Typography extends Component {
   render() {
     const {
-      style,
-      opacity,
-      gradient,
+      h1,
+      h2,
+      h3,
+      title,
+      body,
+      caption,
+      small,
+      size,
+      transform,
+      align,
+      // styling
+      regular,
+      bold,
+      semibold,
+      medium,
+      weight,
+      light,
+      center,
+      right,
+      spacing, // letter-spacing
+      height, // line-height
+      // colors
       color,
-      startColor,
-      endColor,
-      end,
-      start,
-      locations,
-      shadow,
+      accent,
+      primary,
+      secondary,
+      tertiary,
+      black,
+      white,
+      gray,
+      gray2,
+      style,
       children,
       ...props
     } = this.props;
 
-    const buttonStyles = [
-      styles.button,
-      shadow && styles.shadow,
-      color && styles[color], // predefined styles colors for backgroundColor
-      color && !styles[color] && { backgroundColor: color }, // custom backgroundColor
-      style,
+    const textStyles = [
+      styles.text,
+      h1 && styles.h1,
+      h2 && styles.h2,
+      h3 && styles.h3,
+      title && styles.title,
+      body && styles.body,
+      caption && styles.caption,
+      small && styles.small,
+      size && { fontSize: size },
+      transform && { textTransform: transform },
+      align && { textAlign: align },
+      height && { lineHeight: height },
+      spacing && { letterSpacing: spacing },
+      weight && { fontWeight: weight },
+      regular && styles.regular,
+      bold && styles.bold,
+      semibold && styles.semibold,
+      medium && styles.medium,
+      light && styles.light,
+      center && styles.center,
+      right && styles.right,
+      color && styles[color],
+      color && !styles[color] && { color },
+      // color shortcuts
+      accent && styles.accent,
+      primary && styles.primary,
+      secondary && styles.secondary,
+      tertiary && styles.tertiary,
+      black && styles.black,
+      white && styles.white,
+      gray && styles.gray,
+      gray2 && styles.gray2,
+      style, // rewrite predefined styles
     ];
 
-    if (gradient) {
-      return (
-        <TouchableOpacity
-          style={buttonStyles}
-          activeOpacity={opacity}
-          {...props}
-        >
-          <LinearGradient
-            start={start}
-            end={end}
-            locations={locations}
-            style={buttonStyles}
-            colors={[startColor, endColor]}
-          >
-            {children}
-          </LinearGradient>
-        </TouchableOpacity>
-      );
-    }
-
     return (
-      <TouchableOpacity
-        style={buttonStyles}
-        activeOpacity={opacity || 0.8}
-        {...props}
-      >
+      <Text style={textStyles} {...props}>
         {children}
-      </TouchableOpacity>
+      </Text>
     );
   }
 }
 
-Button.defaultProps = {
-  startColor: theme.colors.primary,
-  endColor: theme.colors.secondary,
-  start: { x: 0, y: 0 },
-  end: { x: 1, y: 1 },
-  locations: [0.1, 0.9],
-  opacity: 0.8,
-  color: theme.colors.white,
-};
-
-export default Button;
-
 const styles = StyleSheet.create({
-  button: {
-    borderRadius: theme.sizes.radius,
-    height: theme.sizes.base * 3,
-    justifyContent: "center",
-    marginVertical: theme.sizes.padding / 3,
+  // default style
+  text: {
+    fontSize: theme.sizes.font,
+    color: theme.colors.black,
   },
-  shadow: {
-    shadowColor: theme.colors.black,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
+  // variations
+  regular: {
+    fontWeight: "normal",
   },
-  accent: { backgroundColor: theme.colors.accent },
-  primary: { backgroundColor: theme.colors.primary },
-  secondary: { backgroundColor: theme.colors.secondary },
-  tertiary: { backgroundColor: theme.colors.tertiary },
-  black: { backgroundColor: theme.colors.black },
-  white: { backgroundColor: theme.colors.white },
-  gray: { backgroundColor: theme.colors.gray },
-  gray2: { backgroundColor: theme.colors.gray2 },
-  gray3: { backgroundColor: theme.colors.gray3 },
-  gray4: { backgroundColor: theme.colors.gray4 },
+  bold: {
+    fontWeight: "bold",
+  },
+  semibold: {
+    fontWeight: "500",
+  },
+  medium: {
+    fontWeight: "500",
+  },
+  light: {
+    fontWeight: "200",
+  },
+  // position
+  center: { textAlign: "center" },
+  right: { textAlign: "right" },
+  // colors
+  accent: { color: theme.colors.accent },
+  primary: { color: theme.colors.primary },
+  secondary: { color: theme.colors.secondary },
+  tertiary: { color: theme.colors.tertiary },
+  black: { color: theme.colors.black },
+  white: { color: theme.colors.white },
+  gray: { color: theme.colors.gray },
+  gray2: { color: theme.colors.gray2 },
+  // fonts
+  h1: theme.fonts.h1,
+  h2: theme.fonts.h2,
+  h3: theme.fonts.h3,
+  title: theme.fonts.title,
+  body: theme.fonts.body,
+  caption: theme.fonts.caption,
+  small: theme.fonts.small,
 });
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  View,
   Animated,
   Dimensions,
   Image,
@@ -9,54 +10,20 @@ import {
   ScrollView,
 } from "react-native";
 
-import navigation from "react-native";
-
-import { Button } from "../components/Button";
-import { Block } from "../components/Block";
 import { Text } from "../components/Text";
-import { theme } from "../constants";
 
-export default Welcome = ({ navigation }) => {
+export default function Welcome() {
   return (
-    <Block>
-      <Block center bottom flex={0.4}>
-        <Text h1 center bold>
-          Your Home.
-          <Text h1 primary>
-            {" "}
-            Greener.
-          </Text>
-        </Text>
-        <Text h3 gray2 style={{ marginTop: theme.sizes.padding / 2 }}>
-          Enjoy the experience.
-        </Text>
-      </Block>
-      <Block center middle></Block>
-      <Block middle flex={0.5} margin={[0, theme.sizes.padding * 2]}>
-        <Button gradient onPress={() => navigation.navigate("Login")}>
-          <Text center semibold white>
-            Login
-          </Text>
-        </Button>
-        <Button shadow onPress={() => navigation.navigate("SignUp")}>
-          <Text center semibold>
-            Signup
-          </Text>
-        </Button>
-        <Button onPress={() => this.setState({ showTerms: true })}>
-          <Text center caption gray>
-            Terms of service
-          </Text>
-        </Button>
-      </Block>
-    </Block>
+    <View>
+      <Text>asdasd</Text>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   stepsContainer: {
     position: "absolute",
-    bottom: theme.sizes.base * 3,
+
     right: 0,
     left: 0,
   },
